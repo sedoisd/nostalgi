4285abf12731cfe1b6da3791febf18bc738a0928
sedoisd <saraskin.serafim@yandex.ru>
2025-03-22 16:58:21 +0400

    наброски

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import make_response, jsonify
+from flask import make_response, jsonify, request
 
 from . import db_session
 from .jobs import Jobs
@@ -11,7 +11,7 @@ blueprint = flask.Blueprint(
 )
 
 
-@blueprint.route('/api/jobs')
+@blueprint.route('/api/jobs', methods=['GET'])
 def get_jobs():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs)
@@ -23,10 +23,29 @@ def get_jobs():
 
 
 @blueprint.route('/api/jobs/<int:job_id>', methods=['GET'])
-def get_one_news(job_id):
+def get_one_job(job_id):
     db_sess = db_session.create_session()
     job = db_sess.query(Jobs).get(job_id)
     if not job:
         return make_response(jsonify({'error': 'Not found'}), 404)
     return jsonify({'job': job.to_dict(
         only=('id', 'team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished'))})
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_job():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['title', 'content', 'user_id', 'is_private']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    news = News(
+        title=request.json['title'],
+        content=request.json['content'],
+        user_id=request.json['user_id'],
+        is_private=request.json['is_private']
+    )
+    db_sess.add(news)
+    db_sess.commit()
+    return jsonify({'id': news.id})
