9a173d6bca449274cd13de6e2c1d4545bb755386
sedoisd <saraskin.serafim@yandex.ru>
2025-03-27 23:23:34 +0400

    jobs api edit

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -74,25 +74,32 @@ def delete_news(job_id):
 def edit_job(job_id):
     if not request.json:
         return make_response(jsonify({'error': 'Empty request'}), 400)
-    try:
-        team_leader = request.json['team_leader']
-        job = request.json['job']
-        work_size = request.json['work_size']
-        collaborators = request.json['collaborators']
-        is_finished = request.json['is_finished']
-        # print(request.json)
-        if not (isinstance(team_leader, int) and isinstance(work_size, int) and isinstance(is_finished, bool)):
-            raise Exception
-    except Exception:
-        return make_response(jsonify({'error': 'Bad request'}), 400)
     db_sess = db_session.create_session()
-    news = Jobs(
-        team_leader=team_leader,
-        job=job,
-        work_size=work_size,
-        collaborators=collaborators,
-        is_finished=is_finished
-    )
-    db_sess.add(news)
+    job = db_sess.query(Jobs).filter(Jobs.id == job_id).first()
+    if not job:
+        return make_response(jsonify({'error': 'Not found job'}), 404)
+
+    keys_and_type = [('team_leader', int), ('job', str), ('work_size', int), ('collaborators', str),
+                     ('is_finished', bool)]
+    modified_keys_and_values = dict()
+    for key, correct_type in keys_and_type:
+        value = request.json.get(key, None)
+        if value is not None:
+            if not isinstance(value, correct_type):
+                return make_response(jsonify({'error': 'Bad request'}), 400)
+            modified_keys_and_values[key] = value
+    if len(modified_keys_and_values) != len(request.json):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    # print(modified_keys_and_values, request.json.keys())
+    if 'team_leader' in modified_keys_and_values:
+        job.team_leader = modified_keys_and_values['team_leader']
+    elif 'job' in modified_keys_and_values:
+        job.job = modified_keys_and_values['job']
+    elif 'work_size' in modified_keys_and_values:
+        job.work_size = modified_keys_and_values['work_size']
+    elif 'collaborators' in modified_keys_and_values:
+        job.collaborators = modified_keys_and_values['collaborators']
+    elif 'is_finished' in modified_keys_and_values:
+        job.is_finished = modified_keys_and_values['is_finished']
     db_sess.commit()
-    return jsonify({'id': news.id})
+    return jsonify({'success': 'OK'})
--- test.py
+++ test.py
@@ -9,13 +9,27 @@ from requests import get, post, delete
 #     except Exception:
 #         return response_add_job
 
+def edit_job(id=None, json=None):
+    response_edit_job = post(f'{server}edit_jobs/{id}', json=json)
+    try:
+        return response_edit_job, response_edit_job.json()
+    except Exception:
+        return response_edit_job
+
+edit_json_1 = {'team_leader': 2} # Корректный запрос
+edit_json_2 = {'job': 'setup uav'} # Корректный запрос
+edit_json_3 = {'kartoshka': 'belen'} # Bad request - неизвестный ключ
+edit_json_4 = {'work_size': 'second'} # Bad request - ошибка типа данных
+
+
 server = 'http://localhost:8080/api/'
 response_all_jobs = get(f'{server}jobs')
 
-print(delete(f'{server}jobs/4').json())
-print(delete(f'{server}jobs/4').json())
-print(delete(f'{server}jobs/5').json())
-print(delete(f'{server}jobs/6').json())
+print(edit_job(6, edit_json_1))
+print(edit_job(5, edit_json_2))
+print(edit_job(4, edit_json_3))
+print(edit_job(6, edit_json_4))
 
-print(f'all jobs - {response_all_jobs}')
-pprint.pprint(response_all_jobs.json())
+
+# print(f'all jobs - {response_all_jobs}')
+# pprint.pprint(response_all_jobs.json())
